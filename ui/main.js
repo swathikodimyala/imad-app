5598cadc189ec60ec64926d04c4245f7e58f6d63
swathikodimyala <chinthalaswathi@gmail.com>
2018-03-02 22:03:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,26 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
 
-//change the text of the main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML='santosh';
-
-//move the image
-var img=document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft=marginLeft + 10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick = function(){
-    var interval=setInterval(moveRight, 100);
+button.onclick = function() {
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+     //capture the response and store in the a variable
+     request.onreadystatechange=function(){
+         if(request.readystate==XMLHttpRequest.DONE){
+             //take some action
+             if(request.status==200){
+                 var counter= request.responseText;
+                 var span = document.getElementById('count');
+                 span.innerHtml=counter.toString();
+                 
+             }
+         }
+     
+        
+     //make a request
+     request.open('GET','http://chinthalaswathi.imad.hasura-app.io/counter',true);
+     request.send(null);
+    };
+    
 }
