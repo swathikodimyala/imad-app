a7ecdb9101a7668cfd52b724f36b633b55df4e5d
swathikodimyala <chinthalaswathi@gmail.com>
2018-03-02 20:22:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter',function(req,res)
+{
+    counter = counter + 1;
+    res.send(counter.tostring());
+});
+
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
