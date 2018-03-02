0993c4de986cfedbbe64169c95187d92d6521218
swathikodimyala <chinthalaswathi@gmail.com>
2018-03-02 22:07:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,21 +6,22 @@ button.onclick = function() {
     //create a request object
     var request = new XMLHttpRequest();
      //capture the response and store in the a variable
-     request.onreadystatechange=function(){
-         if(request.readystate==XMLHttpRequest.DONE){
+     request.onreadystatechange = function () {
+         if(request.readystate == XMLHttpRequest.DONE) {
              //take some action
-             if(request.status==200){
-                 var counter= request.responseText;
+             if(request.status == 200) {
+                 var counter = request.responseText;
                  var span = document.getElementById('count');
-                 span.innerHtml=counter.toString();
+                 span.innerHtml = counter.toString();
                  
              }
          }
      
-        
+     };
+     
      //make a request
      request.open('GET','http://chinthalaswathi.imad.hasura-app.io/counter',true);
      request.send(null);
-    };
+   
     
-}
+};
